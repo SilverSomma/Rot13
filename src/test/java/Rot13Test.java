2fd2c02adf1f025f1a6da4dd322c6d09ceb09619
vali-it <asaas>
2023-03-15 17:06:02 +0200

    Parametrized test added.

--- src/test/java/Rot13Test.java
+++ src/test/java/Rot13Test.java
@@ -1,22 +1,32 @@
 import org.example.Rot13;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.util.Random;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class Rot13Test {
     Rot13 rot13 = new Rot13();
+    String random = generateRandomString(50);
 
-    @Test
-    void Rot13() {
-        String original = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-        String encrypted = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
-        assertEquals(encrypted, rot13.rot13(original));
-
-        assertEquals("Guvf    grfg  ---- vffFF n YVGGYR ovvggg PEENNNNNMM   LLLLLLLLLLLllll!!!! VF vg jbEXVVVat?????????",
-                rot13.rot13("This    test  ---- issSS a LITTLE biittt CRRAAAAAZZ   YYYYYYYYYYYyyyy!!!! IS it woRKIIIng?????????"));
-
-        assertEquals("GreR!", rot13.rot13("TerE!"));
-        assertEquals("TerE!", rot13.rot13("GreR!"));
+    private String generateRandomString(int stringLength) {
+        Random random = new Random();
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < stringLength; i++) {
+            stringBuilder.append((char) random.nextInt(32, 127));
+        }
+        return stringBuilder.toString();
     }
 
+    @ParameterizedTest
+    @CsvSource({
+            "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz, NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm",
+            "Guvf    grfg  ---- vffFF n YVGGYR ovvggg PEENNNNNMM   LLLLLLLLLLLllll!!!! VF vg jbEXVVVat?????????, This    test  ---- issSS a LITTLE biittt CRRAAAAAZZ   YYYYYYYYYYYyyyy!!!! IS it woRKIIIng?????????",
+            "TerE!, GreR!"
+    })
+    void Rot13(String input, String expected) {
+        assertEquals(expected, rot13.rot13(input));
+        assertEquals(random, rot13.rot13(rot13.rot13(random)));
+    }
 }
