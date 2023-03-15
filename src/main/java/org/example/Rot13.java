1b477673ec32e8c5d0910ca3405aa922f45bf666
vali-it <asaas>
2023-03-15 12:22:09 +0200

    Methods + basic test done and working as expected.

--- src/main/java/org/example/Rot13.java
+++ src/main/java/org/example/Rot13.java
@@ -1,11 +1,57 @@
 package org.example;
 
+
 public class Rot13 {
+
+    private static final int CYPHER_CONSTANT = 13;
+    private static final int ALPHABET_LENGTH = 26;
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Rot13 rot13 = new Rot13();
+        rot13.encrypt("ABC");
+
     }
 
     public String encrypt(String original) {
-        return null;
+        char[] input = original.toCharArray();
+        StringBuilder result = new StringBuilder();
+
+        for (char character : input) {
+
+            if (isUppercase(character)) {
+                if ((int) character + CYPHER_CONSTANT > 'Z') {
+                    result.append((char) ((int) character + CYPHER_CONSTANT - ALPHABET_LENGTH));
+                } else {
+                    result.append((char) ((int) character + CYPHER_CONSTANT));
+                }
+            } else if (isLowercase(character)) {
+                if ((int) character + CYPHER_CONSTANT > 'z') {
+                    result.append((char) ((int) character + CYPHER_CONSTANT - ALPHABET_LENGTH));
+                } else {
+                    result.append((char) ((int) character + CYPHER_CONSTANT));
+                }
+            } else {
+                result.append(character);
+            }
+        }
+        System.out.println(result.toString());
+        return result.toString();
     }
+
+    boolean isUppercase(char character) {
+        if ((int) character >= 'A' && (int) character <= 'Z') {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    boolean isLowercase(char character) {
+        if ((int) character >= 'a' && (int) character <= 'z') {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
 }
